3cd056e329da3e87ec7cb9baa3e42861641db244
Keanelek Enns <keanelekenns@gmail.com>
2021-02-21 01:46:41 -0800

    initial commit

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("START");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("UPDATE");
+    }
+
+    // Called at consistent intervals (meant for physics)
+    void FixedUpdate()
+    {
+        playerRB.AddForce(0, -10, 50);
+    }
+}
